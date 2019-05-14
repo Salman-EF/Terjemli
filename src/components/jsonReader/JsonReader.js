ed584b34edd0e7cd1cf6a5eb17a493cbfd7b728c
SALMAN <elfadili.salman@gmail.com>
2019-05-14 12:02:01 +0000

    Setup JsonReader component and install google-translate-api

---
+++ src/components/jsonReader/JsonReader.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react'
+
+class JsonReader extends Component {
+
+    componentDidMount() {
+        console.log('JsonReader is here')
+    }
+
+    render() {
+        return (
+            <div>JsonReader is here</div>
+        )
+    }
+}
+
+export default JsonReader
