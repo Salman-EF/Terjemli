aded850eb5bdc1567901f59f64f66e01dfc6ff49
SALMAN <elfadili.salman@gmail.com>
2019-06-23 00:25:36 +0100

    FileReader

--- src/components/jsonReader/JsonReader.js
+++ src/components/jsonReader/JsonReader.js
@@ -1,13 +1,60 @@
-import React, {Component} from 'react'
+import React, {Component} from 'react';
+import './JsonReader.scss';
 
 class JsonReader extends Component {
 
-    componentDidMount() {
+    constructor() {
+        super();
+        this.fileReader = new FileReader();
+        this.state = {
+            isJson: null,
+            file: null
+        }
+    }
+
+    formatVerify = (file) => {
+        if (file && file.type === "application/json") {
+            this.setState({
+                isJson: true,
+                file: file
+            })
+        } else {
+            this.setState({
+                isJson: false
+            })
+        }
+    }
+
+    fileUpload = () => {
+        if (this.state.isJson) {
+            this.fileReader.readAsText(this.state.file);
+            this.fileReader.onload = (event) => {
+                const content = JSON.parse(event.target.result);
+                console.log(content)
+            };
+        } else {
+            console.log("No file")
+        }
+    }
+    readContent = (e) => {
+        const content = e.target.result;
+        console.log(content)
     }
 
     render() {
         return (
-            <div>JsonReader is here</div>
+            <div className="page">
+                <label htmlFor="file">Json File</label>
+                <input id="file" type="file" accept="application/json" 
+                        onChange={e => this.formatVerify(e.target.files[0])} />
+                { this.state.isJson ? (
+                    <div className="json">Json</div>
+                ) : (
+                    <div className="not-json">Not Json</div>
+                )
+                }
+                <button onClick={this.fileUpload}>Translate</button>
+            </div>
         )
     }
 }
